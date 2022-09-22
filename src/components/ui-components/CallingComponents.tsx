33711783b70311fc92dab186aa2a806a9c661281
dmceachernmsft <dmceachern@microsoft.com>
2022-09-21 17:07:20 -0700

    seperate into unique components

--- src/components/ui-components/CallingComponents.tsx
+++ src/components/ui-components/CallingComponents.tsx
@@ -5,15 +5,8 @@ import { useCallback, useEffect, useState } from 'react';
 
 function CallingComponents(): JSX.Element {
     const adapter = useAdapter();
-
-    const videoGalleryProps = usePropsForComposite(VideoGallery);
-    const cameraProps = usePropsForComposite(CameraButton);
-    const microphoneProps = usePropsForComposite(MicrophoneButton);
-    const screenShareProps = usePropsForComposite(ScreenShareButton);
-
-    const onToggleMic = useCallback(async () => {
-        adapter.getState().isLocalPreviewMicrophoneEnabled ? adapter.mute() : adapter.unmute();
-    }, [adapter]);
+    
+    console.log(adapter);
 
     const [page, setPage] = useState<CallCompositePage>(adapter.getState().page);
 
@@ -35,81 +28,106 @@ function CallingComponents(): JSX.Element {
     }
 
     if (page === 'configuration') {
-        const devices = adapter.getState().devices;
-        const localView = adapter.getState().devices.unparentedViews
-        const startCallHandler = () => {
-            adapter.joinCall();
-        }
-        return (
-            <Stack styles={{ root: { margin: 'auto' } }}>
-                <h2>Start a call</h2>
-                <Label>Configure your devices</Label>
-                <Stack horizontal>
-                    <Stack styles={{ root: { width: '30rem', height: '30rem', padding: '2rem' } }}>
-                        <div className={mergeStyles({ height: '100vh' })}>
-                            <VideoTile
-                                renderElement={localView.length > 0 ? (
-                                    <StreamMedia videoStreamElement={
-                                        // this is something that should be better documented in the adapter line 35 as well
-                                        localView.length > 0 && localView[0].view ?
-                                            localView[0].view.target : null} />) : undefined
-                                }
-                                onRenderPlaceholder={() => {
-                                    return (
-                                        <Label styles={{ root: { margin: 'auto' } }}>Your video is off</Label>
-                                    )
-                                }}
-                            />
-                        </div>
-                        <ControlBar layout='floatingTop' >
-                            <MicrophoneButton
-                                {...microphoneProps}
-                                disabled={!devices.deviceAccess?.audio}
-                                onToggleMicrophone={onToggleMic}
-                                checked={adapter.getState().isLocalPreviewMicrophoneEnabled}
-                                showLabel={true}
+        return (<Configuration />);
+    }
 
-                            />
-                            <CameraButton {...cameraProps} showLabel={true} />
-                        </ControlBar>
+    
+    return (<CallScreen />);
+}
+
+function Configuration(): JSX.Element {
+    const cameraProps = usePropsForComposite(CameraButton);
+    const microphoneProps = usePropsForComposite(MicrophoneButton);
+    const adapter = useAdapter();
+    const devices = adapter.getState().devices;
+    const localView = adapter.getState().devices.unparentedViews
+    const startCallHandler = () => {
+        adapter.joinCall();
+    }
+
+    const onToggleMic = useCallback(async () => {
+        adapter.getState().isLocalPreviewMicrophoneEnabled ? adapter.mute() : adapter.unmute();
+    }, [adapter]);
+
+    return (
+        <Stack styles={{ root: { margin: 'auto' } }}>
+            <h2>Start a call</h2>
+            <Label>Configure your devices</Label>
+            <Stack horizontal>
+                <Stack styles={{ root: { width: '30rem', height: '30rem', padding: '2rem' } }}>
+                    <div className={mergeStyles({ height: '100vh' })}>
+                        <VideoTile
+                            renderElement={localView.length > 0 ? (
+                                <StreamMedia videoStreamElement={
+                                    // this is something that should be better documented in the adapter line 35 as well
+                                    localView.length > 0 && localView[0].view ?
+                                        localView[0].view.target : null} />) : undefined
+                            }
+                            onRenderPlaceholder={() => {
+                                return (
+                                    <Label styles={{ root: { margin: 'auto' } }}>Your video is off</Label>
+                                )
+                            }}
+                        />
+                    </div>
+                    <ControlBar layout='floatingTop' >
+                        <MicrophoneButton
+                            {...microphoneProps}
+                            disabled={!devices.deviceAccess?.audio}
+                            onToggleMicrophone={onToggleMic}
+                            showLabel={true}
+
+                        />
+                        <CameraButton
+                            {...cameraProps}
+                            showLabel={true}
+                        />
+                    </ControlBar>
+                </Stack>
+                <Stack styles={{ root: { width: '13rem', padding: '1rem', margin: 'auto' } }}>
+                    <Stack styles={{ root: { padding: '0.5rem' } }}>
+                        <Label>Select your camera</Label>
+                        <Dropdown
+                            defaultSelectedKey={devices.selectedCamera?.id}
+                            options={getDropDownList(devices.cameras)}
+                            onChange={(event, option, index) => {
+                                adapter.setCamera(devices.cameras[index ?? 0])
+                            }}
+                        ></Dropdown>
                     </Stack>
-                    <Stack styles={{ root: { width: '13rem', padding: '1rem', margin: 'auto' } }}>
-                        <Stack styles={{ root: { padding: '0.5rem' } }}>
-                            <Label>Select your camera</Label>
-                            <Dropdown
-                                defaultSelectedKey={devices.selectedCamera?.id}
-                                options={getDropDownList(devices.cameras)}
-                                onChange={(event, option, index) => {
-                                    adapter.setCamera(devices.cameras[index ?? 0])
-                                }}
-                            ></Dropdown>
-                        </Stack>
-                        <Stack styles={{ root: { padding: '0.5rem' } }}>
-                            <Label>Select your microphone</Label>
-                            <Dropdown
-                                defaultSelectedKey={devices.selectedMicrophone?.id}
-                                options={getDropDownList(devices.microphones)}
-                                onChange={(event, option, index) => {
-                                    adapter.setMicrophone(devices.microphones[index ?? 0])
-                                }}
-                            ></Dropdown>
-                        </Stack>
-                        <Stack styles={{ root: { padding: '0.5rem' } }}>
-                            <Label>Select your speaker</Label>
-                            <Dropdown
-                                defaultSelectedKey={devices.selectedSpeaker?.id}
-                                options={getDropDownList(devices.speakers)}
-                                onChange={(event, option, index) => {
-                                    adapter.setSpeaker(devices.speakers[index ?? 0])
-                                }}
-                            ></Dropdown>
-                        </Stack>
-                        <PrimaryButton styles={{ root: { width: '12rem', marginTop: '2rem' } }} onClick={startCallHandler}>Start Call</PrimaryButton>
+                    <Stack styles={{ root: { padding: '0.5rem' } }}>
+                        <Label>Select your microphone</Label>
+                        <Dropdown
+                            defaultSelectedKey={devices.selectedMicrophone?.id}
+                            options={getDropDownList(devices.microphones)}
+                            onChange={(event, option, index) => {
+                                adapter.setMicrophone(devices.microphones[index ?? 0])
+                            }}
+                        ></Dropdown>
                     </Stack>
+                    <Stack styles={{ root: { padding: '0.5rem' } }}>
+                        <Label>Select your speaker</Label>
+                        <Dropdown
+                            defaultSelectedKey={devices.selectedSpeaker?.id}
+                            options={getDropDownList(devices.speakers)}
+                            onChange={(event, option, index) => {
+                                adapter.setSpeaker(devices.speakers[index ?? 0])
+                            }}
+                        ></Dropdown>
+                    </Stack>
+                    <PrimaryButton styles={{ root: { width: '12rem', marginTop: '2rem' } }} onClick={startCallHandler}>Start Call</PrimaryButton>
                 </Stack>
             </Stack>
-        )
-    }
+        </Stack>
+    )
+}
+
+function CallScreen(): JSX.Element {
+    const adapter = useAdapter();
+    const videoGalleryProps = usePropsForComposite(VideoGallery);
+    const cameraProps = usePropsForComposite(CameraButton);
+    const microphoneProps = usePropsForComposite(MicrophoneButton);
+    const screenShareProps = usePropsForComposite(ScreenShareButton);
 
     return (
         <Stack className={mergeStyles({ height: '30rem' })}>
@@ -127,7 +145,6 @@ function CallingComponents(): JSX.Element {
     );
 }
 
-export default CallingComponents;
 
 const getDropDownList = (list: Array<VideoDeviceInfo | AudioDeviceInfo>): IDropdownOption[] => {
     const dropdownList: IDropdownOption[] = [];
@@ -145,3 +162,5 @@ const getDropDownList = (list: Array<VideoDeviceInfo | AudioDeviceInfo>): IDropd
 
     return dropdownList;
 }
+
+export default CallingComponents;
